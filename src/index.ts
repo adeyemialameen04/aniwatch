c4f728f073a315f8f1eeb318d8c3f14a241964c3
adeyemialameen04 <adeyemialameen04@gmail.com>
2024-10-22 01:09:13 +0100

    feat: created home route and EDEN

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ import { Elysia } from "elysia";
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+	`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
+export type App = typeof app;
